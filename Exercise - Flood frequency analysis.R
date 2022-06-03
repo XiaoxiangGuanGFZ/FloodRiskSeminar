8ae3aa3dea2a1873563d6882fbb3ce584fedbba3
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2022-06-03 08:19:00 +0200

    update flood frequency analysis exercise

--- Exercise - Flood frequency analysis.R
+++ Exercise - Flood frequency analysis.R
@@ -6,74 +6,99 @@
 # Last updated: 
 # Xiaoxiang Guan (guan.xiaoxiang@gfz-potsdam.de)
 
+# typical data analysis procedure in R project 
+# 1. data import
+# 2. data check and data clean
+# 3. data summary and management
+# 4. model the data
+# 5. display the data
+# 6. presentation
+
 # first create project folder and save there the data file in subfolder "Data"
 # then File --> New Project --> Existing directory
 wd <- getwd()
 # getwd() should give now the project directory
-wd <- 'D:/FloodRiskSeminar/Materials from Luzie/'
+wd <- 'D:/FloodRiskSeminar/FloodRisk/data/'
 setwd(wd)
 
-# example how to install packages
-# install.packages("extRemes")
-library(extRemes)
+# install packages required in this project
+install.packages("extRemes")  # package for extreme value statistics
+library(extRemes)  # load the package (objects,functions and codes)
+# to R environment to be applied.
 
+################################ Data preparation ##############################
 
-################################ Data preparation ###########################################
-
-# read data from file
-DailyDischarge = read.table(file = "Data/Koeln_1.txt", header = TRUE, sep = "\t", 
-                            col.names = c("date", "Q"), stringsAsFactors = FALSE)
-
-# adjust date format
-DailyDischarge$date = as.Date(DailyDischarge$date, format = "%d.%m.%Y")
-
-# extract year from date
-DailyDischarge$year = as.numeric(strftime(DailyDischarge$date, format = "%Y"))
-
-
-# POT & AMS
-
+# read (import) data from file
+DailyDischarge = read.table(file = "Koeln-data-Q.csv", 
+                            header = TRUE, sep = ',')
+head(DailyDischarge)  # check the data
+View(DailyDischarge)
 
 # extract annual maximum discharge
-AMS = aggregate(list(Qmax = DailyDischarge$Q), by = list(year = DailyDischarge$year), max)
+AMS = aggregate(Q~year, data = DailyDischarge, FUN = max)
+head(AMS)  # check the results
+
+# then how to extract all the values over a specified threshold? (peak-over-threshold)
 
 ################################~Data preparation~##################################
-ams = AMS
-colnames(ams) <- c('year', 'value')
-ams$m_rank = rank(ams$value) # rank of the values
-ams$P = round(ams$m_rank / (1+dim(ams)[1]), 4) # empirical probability
-ams$ReturnPeriod_T = round(1 / (1-ams$P), 1) # # empirical return period
-df = ams
-plot(log10(df$ReturnPeriod_T), df$value, 
-     #main = 'Return level plot',
-     xlab = 'return period: years',
-     ylab = 'Return level (Discharge: m3/s)',
-     type = 'p',
-     xaxt = "n")
-axis(1, labels = c(1, 10,100,1000),
-     at = log10(c(1, 10,100,1000)))
-grid(nx = 20, 
-     ny = NULL,
+
+colnames(AMS) <- c('year', 'Qmax')
+AMS$m_rank = rank(AMS$Qmax) # rank of the values
+AMS$P = round(AMS$m_rank / (1 + dim(AMS)[1]), 4) # empirical probability
+# dim(AMS)[1]: the first dimension of data frame AMS, 
+#   namely the number of rows (the number of years, the sample size)
+
+# round(x, n): round() function rounds the values to 
+#   the specified number of decimal places (default 0)
+
+AMS$ReturnPeriod_T = round(1 / (1 - AMS$P), 1) # # empirical return period
+
+plot(log10(AMS$ReturnPeriod_T), AMS$Qmax, 
+     # first two parameters: mapping to x and y axis respectively
+     main = 'Return level plot',  # title of the plot
+     xlab = 'return period: years',  # the label of x axis
+     ylab = 'Return level (Discharge: m3/s)',  # the label of y axis
+     type = 'p',  # the type of the plot
+     xaxt = "n"  # hide the tick labels of x axis
+     )
+axis(1, # identity No. of x axis
+     labels = c(1, 10,100,1000), # customized labels
+     at = log10(c(1, 10,100,1000))  # locations the labels to be put
+     )  # customize the tick labels in x axis
+
+grid(nx = 20, # number of x grid lines
+     ny = NULL,  # no grid line at y axis
      lty = 2,      # Grid line type
      col = "gray", # Grid line color
      lwd = 0.5)      # Grid line width
-
 
 ############################ Exploratory data analysis ############################
 
 nrow(AMS) # time series length
 
-plot(AMS$year, AMS$Qmax, type = "l", ylab = "Q_max (m^3/s)", xlab = NA)
-plot(DailyDischarge$Q, type = "l")
+plot(AMS$year, AMS$Qmax, 
+     type = "l", # plot type: line plot
+     main = 'Annual maximum discharge at Koeln',
+     ylab = "Q_max (m^3/s)", 
+     xlab = NA
+     )
+plot(DailyDischarge$Q, # variable for x axis is neglected, 
+     
+     type = "l",
+     col = 'lightblue',
+     main = 'Daily discharge'
+     )
 
 max(AMS$Qmax)
 min(AMS$Qmax)
 mean(AMS$Qmax)  
 median(AMS$Qmax) # median < mean : positively skewed distribution
-hist(AMS$Qmax)
 
-boxplot(AMS$Qmax)
-abline(h = mean(AMS$Qmax), col = "red")
+# basic statistic diagrams
+hist(AMS$Qmax)  # histogram
+boxplot(AMS$Qmax) # boxplot
+abline(h = mean(AMS$Qmax), # add one horizontal line
+       col = "red")
 
 ############################~Exploratory data analysis~############################
 
@@ -81,49 +106,68 @@ abline(h = mean(AMS$Qmax), col = "red")
 
 ############################ Flood frequency analysis ##############################
 
-#### 1. Fit the Generalised Extreme Value distribution (GEV) to the annual maxima ###
+#### 1. Fit the Generalized Extreme Value distribution (GEV) to the annual maxima ###
 
 # load needed package
 library(extRemes)
 
 # fit the GEV
-GEV_mle = fevd(AMS$Qmax, method = "MLE", type = "GEV")
-GEV_mle
+GEV_mle = fevd(AMS$Qmax, # extreme variable
+               method = "Lmoments", # parameter estimation method
+               type = "GEV"  # probability distribution type
+               )
 
-GEV_mle = fevd(AMS$Qmax, method = "MLE", type = "Exponential")
-
-GEV_mle = fevd(AMS$Qmax, method = "Lmoments", type = "GEV")
+GEV_mle = fevd(AMS$Qmax, 
+               method = "MLE", 
+               type = "GEV"
+               )
+GEV_mle  # check the results
 
 # The package extRemes offers the possibility to plot diagnostic plots
-plot.fevd(GEV_mle, type = "primary")
-dev.off()   # return to normal plotting without subplots
 
 plot.fevd(GEV_mle, type = "probprob") # Probability-probability plot
-# the Model vs. Empirical probabilities 
-main = 'Probability plot'
-plot.fevd(GEV_mle, type = "qq") # Quantile-Quantile (QQ) plot
-main = 'Quantile plot'
+# the Model vs. Empirical probabilities; main = 'Probability plot'
+
+plot.fevd(GEV_mle, type = "qq") # Quantile-Quantile (QQ) plot; main = 'Quantile plot'
+
 plot.fevd(GEV_mle, type = "density") # Compare modeled and empirical density function
 
 plot.fevd(GEV_mle, type = "rl")   # Return level plot with confidence intervals (5%, 95%)
-main = 'Return level plot'
+# 'Return level plot'
 
-#### 2.  Compare GEV with Gumbel distribution ###
+
+plot.fevd(GEV_mle, type = "primary")
+dev.off()   # return to normal plotting without subplots
+
+#### 2.  Gumbel distribution ###
 
 # Gumbel is a subcase of GEV with zero shape parameter, which is impossible or very unlikely 
 # to occur when one fits the GEV. 
 
 Gumb_mle = fevd(AMS$Qmax, method = "MLE", type = "Gumbel") # Gumbel has shape = 0, 
 # this means the method uses only two fitting parameters (location and scale)
-
-# compare GEV - Gumbel diagnostic plots
-par(mfcol = c(2, 3))  # enable subplots: 2 rows, 3 columns
-plot.fevd(GEV_mle, type = "density", main = "GEV")
-plot.fevd(Gumb_mle, type = "density", main = "Gumbel")
-
-plot.fevd(GEV_mle, type = "qq", main = NA)
+plot.fevd(Gumb_mle, type = "density", main = "Gumbel, shape == 0")
 plot.fevd(Gumb_mle, type = "qq", main = NA)
-
-plot.fevd(GEV_mle, type = "rl", main = NA)
 plot.fevd(Gumb_mle, type = "rl", main = NA)
+
+#### Exercises
+
+# (1) do the GEV distribution fitting to annual maximum daily rainfall 
+#   and discharge in data file 'Chitan-data-PEQ.csv' respectively. 
+df <- read.table(
+  # fill the parameters
+  'Chitan-data-PEQ.csv'
+)
+
+
+AMS_rainfall <- aggregate()
+
+
+# (2) do the GEV distribution fitting to seasonal extreme daily discharge
+# two seasons in a calendar year: 
+# summer: from May to October
+# Winter: November, December, and January to April
+
+
+
 
