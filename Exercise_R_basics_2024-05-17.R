aa44722aeaaf7432a974324a352f2c935dc5650c
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2024-05-16 17:36:25 +0200

    update

--- Exercise_R_basics_2024-05-17.R
+++ Exercise_R_basics_2024-05-17.R
@@ -10,24 +10,6 @@
 
 ### ------ Introduction to R ------
 
-# packages -------
-install.packages('ggplot2') # install packages(s) into R environment
-install.packages("extRemes")
-
-library('ggplot2') # Load a package into this working environment
-
-help(package="ggplot2") 
-help(package="extRemes") 
-
-# provides a brief description of the package and an
-# index of the functions and datasets included
-
-# getting help in R --------
-
-help(max)  # with a function name
-?max
-
-
 ### ------ Data types and structures in R ------
 ## ---- Data types ----
 
@@ -58,15 +40,13 @@ as.numeric('xs')   # failed
 as.integer(10.54)  # convert to integer
 as.logical(5)      # TRUE: as long as it is a non-zero numerical value 
 
-# convert boolean to integer 
+# convert boolean / logical value to integer 
 
 as.numeric(TRUE)  # 1
 as.numeric(FALSE) # 0
 
 1 + TRUE
-
 2 - TRUE * 2
-
 2 - FALSE
 
 ## ----- Data structures in R --------
@@ -81,33 +61,10 @@ v3 <- c("Name1", "Name2")      # character vector, with 2 elements
 v4 <- 1:5  # a numeric sequence from 1 to 5 with a step of 1
 v4 <- seq(1, 10, by = 2)
 
-# element indexing: access specific elements in the vector
-v1[1]  # access the first element in vector a
-v1[1:3]  # access the first 3 elements in a
-v1[c(1, 3, 5, 7)]  # access multiple elements in the vector with position vector c(1, 3, 5, 7)
-
 
 v5 <- 1:20
 v5 <- seq(from = 1, to = 20, by = 2) # sequence
 
-
-# Matrices ------
-# A matrix is a two-dimensional array 
-# in which each element has the same mode (numeric, character, or logical)
-
-y <- matrix(data = 1:20, nrow=5, ncol=4, byrow = T) # produce matrix from a vector
-y
-
-# Array ------
-# Arrays are similar to matrices but can have more than two dimensions
-dim1 <- c("A1", "A2")
-dim2 <- c("B1", "B2", "B3")
-dim3 <- c("C1", "C2", "C3", "C4")
-z <- array(data = 1:24, 
-           dim = c(2, 3, 4), 
-           dimnames = list(dim1, dim2, dim3)
-           ) # array(vector, dimensions, dimnames)
-z
 
 # data.frame ----
 # A data frame is more general than a matrix in 
@@ -140,7 +97,7 @@ df <- read.table(
   header = TRUE, sep = ','
 )
 
-?read.table
+?read.table  # find more about the function and parameters
 
 write.table(
   df, 
@@ -218,7 +175,7 @@ rownames(df)  # row names of data frame
 summary(df)   # a brief summary of a numeric data frame
 
 
-# Customized functions ----
+# User-defined functions ----
 myFunc <- function(parameter_list) {
   # the operations in this function are performed on or based on
   #    the parameters provided.
@@ -276,11 +233,16 @@ if (condition)
 1.25 >= 2  # greater than or equal to
 
 x = 1
-x == 1  # equal
-x != 1  # not equal 
+x == 1     # equal
+x != 1     # not equal 
 
 
 c(1, 2, 1, 3, 2.5) > 1.5 # perform vectorized comparison 
+c(1, 2, 1, 3, 2.5) == c(1, 0, 1, 0, 0)
+
+is.numeric()
+is.character()
+is.logical()
 
 ## logical operator ---- 
 x1 <- 1.5  
@@ -290,16 +252,18 @@ y2 <- 0.6
 
 #  Logical Operators
 # chain multiple test expressions
-x1 > x2 & y1 < y2   # &: and
-x1 <= x2 & y1 <= y2 # &: and
-x1 > x2 | y1 <= y2  # |: or 
-x1 <= x2 | y1 <= y2 # |: or
+x1 > x2  & y1 < y2   # &: and
+x1 <= x2 & y1 <= y2  
+x1 > x2  | y1 <= y2  # |: or 
+x1 <= x2 | y1 <= y2  
+
+# vectorized operation also applies here, but you need also pay extra attention. 
 
 x1 <= x2
 !(x1 <= x2)         # !: exclamation mark: not 
 
-# if-else example ----
-x = -3
+# if-else examples ----
+x <- -3
 if (x > 0) {
   x = x
 } else if (x < 0) {
@@ -308,7 +272,14 @@ if (x > 0) {
   x = 0
 }
 
-
+x <- c(-3, 1, 2)
+if (x > 0) {  ## not valid here. 
+  x = x
+} else if (x < 0) {
+  x = -x
+} else {
+  x = 0
+}
 
 # ---- loops -------
 Ids <- c(1, 2, 3, 4, 5)
@@ -320,8 +291,14 @@ for (i in Ids) {
 
 id = 1
 while (id <= 5) {
-  print(id)
+  out = id * (-1)
+  print(out)
   id = id + 1
+}
+
+while (id <= 5) {
+  out = id * (-1)
+  print(out)
 }
 
 #### ---- Data manipulation -----
@@ -334,23 +311,19 @@ x <- c(1,2,3,4,5,6)  # numeric vectors
 
 # creating logical vectors
 x <- c(TRUE, FALSE, FALSE)  # direct creation 
-x[2] <- TRUE
-c(x, c(TRUE, FALSE))
 
-x1 <- c(0.1, 1, 1.2, 0.25, 0.56, 5)
-x1 < 0.5  # create from comparison expression 
-x1 > 0.5
+nv <- c(0.1, 1, 1.2, 0.25, 0.56, 5)
+x <- nv < 0.5  # create from comparison expression 
+x <- nv > 0.5
 
 # creating character vectors
 x <- c('a', 'b', 'c', 'f')
-as.character(c(1,2,3))
 
-x[1]<- 'd'
-c(x, 'e', 'd')  # combine
-paste0('s', 'd')  # paste, or concatenate together
+as.character(c(1,2,3))
 
 years = 1999:2020  # convert numeric to string
 paste0('Y', years)
+
 
 # creating data frame (from multiple vectors)
 # import from text files
@@ -361,6 +334,8 @@ diabetes <- c("Type1", "Type2", "Type1", "Type1", "Type1", "Type2")
 status <- c("Poor", "Improved", "Excellent", "Poor", "Poor", "Improved")
 df <- data.frame(patientID, age, diabetes, status)
 df
+
+# data rename -----
 
 colnames(df)  # access the column names
 colnames(df) <- c("c1", "c2", "c3", "c4")  # rename the columns
@@ -380,35 +355,37 @@ x[10] <- 10
 
 x1 <- c(2, 4, 6)
 
-x <- c(x, x1)
+x <- c(x, x1)  # combine with vectors 
 
+# data sorting -----
+x <- c(1.0, 0, -2.2, 2.5, -5.1, 6, 4.15, -1.5, 3.2)
+sort(x)           # sort the numeric vector in increasing order
+sort(x, decreasing = T)
 
 # filtering and sub-setting ------
 
 x1 <- c(1.0, 0, -2.2, 2.5, -5.1, 6, 4.15, -1.5, 3.2)
 
-# select (access) by index (or position)
+# select (subset) by index (or position)
 x1[1]
 x1[6]
 
-# select by an index (position) vector
+# subset by an index (position) vector
 x1[1:6]
 x1[c(1, 3, 5)]
 
-# select by logical expression
-x1[c(TRUE, FALSE, FALSE, FALSE, TRUE, TRUE,FALSE, FALSE, FALSE)]
+# subset by logical expression
+x1[c(TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE)]
 
 x1[x1 > 0] # select all the elements in x1 greater than 0 (positive values)
 
 x1[x1 > mean(x1)]  # select all the elements in x1 greater than the average
 x1[x1 > 0 & x1 < 5] 
 
-x1[-2]  # exclude the 2nd value 
 
 # filter a data frame with two dimension
 # select by a index
 df[1, 1]
-df[3, 2]
 
 # select one row of data (observation) by index
 df[1, ]
@@ -418,11 +395,10 @@ df[, 1]
 
 # select by name
 colnames(df)
-row.names(df)
-df[1, 'age']
-df['1', 'status']
+df[, "age"]
+df[1, "age"]
 
-# multiple selecting
+# multiple selecting (sub-setting)
 df[1:2, 1]
 df[1, 2:3]
 
@@ -440,12 +416,21 @@ df$age[df$age > 18 & df$age < 50]
 
 df$age[df$status != "Poor"]  # filter one column based on another column
 
-length(df$age[df$status == "Poor"])
 max(df$age[df$status == "Poor"])
 mean(df$age[df$status == "Poor"])
 
 
 # data frame merging ------
+
+gender <- c("M", "F", "F", "M", "M", "F")
+cbind(df, gender) # bind by column, number of df rows == length of vector
+
+rbind(
+  df, 
+  c(7, 30, "Type2", "Excellent")
+) # bind by row, number of df columns == length of vector
+
+
 df1 <- data.frame(
   'c1' = 1:3,
   'c2' = c('a', 'b', 'c')
@@ -455,11 +440,8 @@ df2 <- data.frame(
   'c2' = c('d', 'e', 'f')
 )
 
-cbind(df1, df2)  # bind by column, two data frames should have the same number of rows
-rbind(df1, df2)  # bind by row, two data frames should have the same number of columns
-
-cbind(df1, c(4,5,6)) # bind a data frame with a vector (with the same dimension)
-rbind(df1, c(4,'d')) 
+cbind(df1, df2)
+rbind(df1, df2)
 
 df2 = data.frame(
   ID = c(2,3,5), 
@@ -469,9 +451,14 @@ df2 = data.frame(
 merge(
   df, df2,
   by.x = "ID", by.y = "ID", all.x = TRUE
-)
+)  # identity column: ID
+
 
 # data frame group and aggregation ------
+
+aggregate(age~diabetes, df, FUN = mean)
+aggregate(age~diabetes, df, FUN = max)
+aggregate(age~status, df, FUN = mean)
 
 
 ### ------- Exercise with R - R basics -----------
