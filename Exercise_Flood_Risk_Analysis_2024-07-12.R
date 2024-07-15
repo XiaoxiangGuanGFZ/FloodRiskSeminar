cf7b6f7324e39398d40539c52452635b4a546072
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2024-07-15 08:53:24 +0200

    solutions to the exercises

--- Exercise_Flood_Risk_Analysis_2024-07-12.R
+++ Exercise_Flood_Risk_Analysis_2024-07-12.R
@@ -14,10 +14,26 @@
 # use read.table() function, 
 # then derive the annual maximum discharge (AMS) series: aggregate() 
 
+# -- read in data
+df <- read.table(
+  # fill the parameters
+  "D:/FloodRiskSeminar/data/Example_data.csv",
+  header = T, sep = ','
+)
 
+# extract annual maximum discharge with aggregate()
+AMS = aggregate(discharge ~ year, data = df, FUN = max)
 
 # Exercise 1.2 ------
 # Estimate the GEV parameters for AMS: fevd()
+library(extRemes) 
+# fit the GEV by using fevd() function in `extRemes` package
+GEV_mle = fevd(AMS$discharge, # extreme variable
+               method = "MLE", # parameter estimation method
+               type = "GEV"  # probability distribution type
+)
+
+paras <- as.numeric(GEV_mle$results$par)  # the estimated 3 GEV parameters
 
 
 # Exercise 1.3 ------
@@ -25,6 +41,9 @@
 # hint: 
 # the return period is 100, then estimate the probability,
 # then use qevd() function
+rp = 100
+p = 1 - 1 / rp
+qevd(p, loc = paras[1], scale = paras[2], shape = paras[3], type = "GEV")
 
 
 # Exercise 1.4 ------
@@ -32,7 +51,11 @@
 # hint:
 # estimate the return period of random variable taking on 
 # the value of 10000, using qevd() function
+Q = 10000
+p = pevd(Q, loc = paras[1], scale = paras[2], shape = paras[3], type = "GEV")
 
+rp = 1 / (1 - p)
+rp
 
 
 # Exercise 1.5 ------
@@ -41,6 +64,28 @@
 # Hint:
 # Weilbull formula to calculate the empirical probability - plot()
 # Use the 3 GEV parameters to plot the estimated GEV curve - points()
+
+
+AMS$m_rank = rank(AMS$discharge) # rank of the values
+AMS$P = round(AMS$m_rank / (1 + dim(AMS)[1]), 4) # empirical probability
+AMS$ReturnPeriod_T = round(1 / (1 - AMS$P), 1) # # empirical return period
+AMS
+
+plot(x = AMS$ReturnPeriod_T, y = AMS$discharge)
+
+gene_rp <- seq(1.1, 500, 0.5)  ## artifical return periods
+gene_p <- 1 - 1/ gene_rp       ## corresponding probability
+
+gene_q <- qevd(gene_p, loc = paras[1], scale = paras[2], shape = paras[3], type = "GEV")
+# gene_q: the estimated discharges
+
+
+plot(
+  x = AMS$ReturnPeriod_T, y = AMS$discharge,
+  xlab = "return period [year]",
+  ylab = "discharge [m3/s]"
+)  # generate a plot
+points(x = gene_rp, y = gene_q, col = "red", type = "l") # add one layer
 
 
 
